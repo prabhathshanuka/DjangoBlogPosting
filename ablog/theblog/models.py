0c30d49c2b84b48af2f07678417712b1851ce899
shanu <prabhathshanuka@gmail.com>
2020-10-10 22:53:47 +0530

    view data from data base

---
+++ ablog/theblog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+#this class for use as a template of database
+class Post(models.Model):
+	title = models.CharField(max_length=255)
+	author = models.ForeignKey(User, on_delete = models.CASCADE)
+	body = models.TextField()
+
+	def __str__(self):
+		return self.title + ' | ' + str(self.author)
+
---
+++ ablog/theblog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+# Create your views here.
+#def home(request):
+	#return render(request, 'home.html', {})
+	
+class HomeView(ListView):
+	model = Post
+	template_name = 'home.html'
+	
