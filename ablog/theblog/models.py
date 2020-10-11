30b1054dda3ee032d018ea8fe145c1db187aebd0
shanu <prabhathshanuka@gmail.com>
2020-10-11 13:25:00 +0530

    add unique title for every html pages from the database title_tag

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 #this class for use as a template of database
 class Post(models.Model):
 	title = models.CharField(max_length=255)
+	title_tag = models.CharField(max_length=255, default = "Awesome Blog!")
 	author = models.ForeignKey(User, on_delete = models.CASCADE)
 	body = models.TextField()
 
